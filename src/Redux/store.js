3364105a72fd890475f44ba77bc3c77bf57abbae
Nguyen Tien Dung <sparkroy.113@gmail.com>
2022-05-25 08:42:07 +0700

    fix

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,18 +1,40 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { userListReducer, userLoginReducer } from "./Reducers/userReducers";
+import {
+  productCreateReducer,
+  productDeleteReducer,
+  productEditReducer,
+  productListReducer,
+  productUpdateReducer,
+} from "./Reducers/ProductReducers";
+import {
+  orderDeliveredReducer,
+  orderDetailsReducer,
+  orderListReducer,
+} from "./Reducers/OrderReducres";
 
 const reducer = combineReducers({
-  // userLogin: userLoginReducer,
+  userLogin: userLoginReducer,
+  userList: userListReducer,
+  productList: productListReducer,
+  productDelete: productDeleteReducer,
+  productCreate: productCreateReducer,
+  productEdit: productEditReducer,
+  productUpdate: productUpdateReducer,
+  orderList: orderListReducer,
+  orderDetails: orderDetailsReducer,
+  orderDeliver: orderDeliveredReducer,
 });
 
 // login
-// const userInfoFromLocalStorage = localStorage.getItem("userInfo")
-//   ? JSON.parse(localStorage.getItem("userInfo"))
-//   : null;
+const userInfoFromLocalStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
 
 const initialState = {
-  // userLogin: { userInfo: userInfoFromLocalStorage },
+  userLogin: { userInfo: userInfoFromLocalStorage },
 };
 
 const middleware = [thunk];
